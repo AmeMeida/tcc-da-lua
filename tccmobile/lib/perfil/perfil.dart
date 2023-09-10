1df30200bc3ae4cb21d68e7bd630668df7b4c822
Lua-da-tarde <luanna@paggiaro.com.br>
2023-09-10 20:53:41 -0300

    perfil pronto praticamente

--- tccmobile/lib/perfil/perfil.dart
+++ tccmobile/lib/perfil/perfil.dart
@@ -40,13 +40,21 @@ class _PerfilState extends State<Perfil> {
     print("salvou");
   }
 
-  Icon mudaIcon(){
-  if(enabled == true){
-    return Icon(Icons.save, color: Cores.branco, size: 25,);
-  }else{
-    return Icon(Icons.edit, color: Cores.branco, size: 25,);
+  Icon mudaIcon() {
+    if (enabled == true) {
+      return Icon(
+        Icons.save,
+        color: Cores.branco,
+        size: 25,
+      );
+    } else {
+      return Icon(
+        Icons.edit,
+        color: Cores.branco,
+        size: 25,
+      );
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -71,96 +79,182 @@ class _PerfilState extends State<Perfil> {
                     ),
                   ),
                   Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Cores.azulLogo,
-                        shape: BoxShape.circle
-                      ),
-                      child: IconButton(
-                        icon: Icon(Icons.photo_camera),
-                        color: Cores.branco,
-                        iconSize: 20,
-                        onPressed: (){},
-                      ),
-                    )
-                  )
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Cores.azulLogo, shape: BoxShape.circle),
+                        child: IconButton(
+                          icon: Icon(Icons.photo_camera),
+                          color: Cores.branco,
+                          iconSize: 20,
+                          onPressed: () {},
+                        ),
+                      ))
                 ],
               ),
               SizedBox(height: 10),
               Form(
-                key: _formKey,
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _campoNome,
-                        enabled: enabled,
-                        decoration: InputDecoration(
-                          labelText: "Nome completo",
-                          prefixIcon: const Icon(Icons.person),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
+                  key: _formKey,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _campoNome,
+                          enabled: enabled,
+                          decoration: InputDecoration(
+                            labelText: "Nome completo",
+                            prefixIcon: const Icon(Icons.person),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      MaskedTextField(
-                        controller: _campoEmail,
-                        enabled: enabled,
-                        decoration: InputDecoration(
-                          labelText: "E-mail",
-                          prefixIcon: const Icon(Icons.email),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
+                        SizedBox(height: 20),
+                        MaskedTextField(
+                          controller: _campoEmail,
+                          enabled: enabled,
+                          decoration: InputDecoration(
+                            labelText: "E-mail",
+                            prefixIcon: const Icon(Icons.email),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      MaskedTextField(
-                        controller: _campoCel,
-                        enabled: enabled,
-                        decoration: InputDecoration(
-                          labelText: "Celular",
-                          prefixIcon: const Icon(Icons.phone),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
+                        SizedBox(height: 20),
+                        MaskedTextField(
+                          controller: _campoCel,
+                          enabled: enabled,
+                          mask: '(##) #####-####',
+                          decoration: InputDecoration(
+                            labelText: "Celular",
+                            prefixIcon: const Icon(Icons.phone),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      MaskedTextField(
-                        controller: _campoNasc,
-                        enabled: enabled,
-                        decoration: InputDecoration(
-                          labelText: "Data de Nascimento",
-                          prefixIcon: const Icon(Icons.calendar_today),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
+                        SizedBox(height: 20),
+                        MaskedTextField(
+                          controller: _campoNasc,
+                          enabled: enabled,
+                          mask: "##/##/####",
+                          decoration: InputDecoration(
+                            labelText: "Data de Nascimento",
+                            prefixIcon: const Icon(Icons.calendar_today),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                    ],
-                  ),
-                )
-              )
+                        SizedBox(height: 20),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            "Deficiências:",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Cores.azul,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        IgnorePointer(
+                          ignoring: !enabled,
+                          child: Opacity(
+                            opacity: enabled ? 1 : 0.5,
+                            child: Column(
+                              children: [
+                                //Auditiva
+                                Row(children: [
+                                  Checkbox(
+                                    value: auditiva,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        auditiva = value!;
+                                        print(auditiva);
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    "Auditiva",
+                                    style: TextStyle(
+                                        fontSize: 12, fontFamily: "Comfortaa"),
+                                  ),
+                                ]),
+                                //Visual
+                                Row(children: [
+                                  Checkbox(
+                                    value: visual,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        visual = value!;
+                                        print(visual);
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    "Visual",
+                                    style: TextStyle(
+                                        fontSize: 12, fontFamily: "Comfortaa"),
+                                  ),
+                                ]),
+                                //Motora
+                                Row(children: [
+                                  Checkbox(
+                                    value: motora,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        motora = value!;
+                                        print(motora);
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    "Motora",
+                                    style: TextStyle(
+                                        fontSize: 12, fontFamily: "Comfortaa"),
+                                  ),
+                                ]),
+                                //Outra
+                                Row(children: [
+                                  Checkbox(
+                                    value: outra,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        outra = value!;
+                                        print(outra);
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    "Outra",
+                                    style: TextStyle(
+                                        fontSize: 12, fontFamily: "Comfortaa"),
+                                  ),
+                                ])
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ))
             ],
           ),
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            backgroundColor: Cores.vermelho,
-            child: mudaIcon(),
-            onPressed: () => setState(Alterar),
-          )
-        ]
-      ),
+      floatingActionButton:
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+          backgroundColor: Cores.vermelho,
+          child: mudaIcon(),
+          onPressed: () => setState(Alterar),
+        )
+      ]),
     );
   }
 }
